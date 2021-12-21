b464a9eb2e531cb213545b8b51828ce0ad77ad94
Sergey Simashov <simashov@gmail.com>
2021-12-21 22:07:03 +0300

    Lesson 1

---
+++ Patterns/Delegate/DelegateSecondViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DelegateSecondViewController.swift
+//  Patterns
+//
+//  Created by Sergey Simashov on 21.12.2021.
+//
+
+import UIKit
+
+class DelegateSecondViewController: UIViewController {
+
+    @IBOutlet weak var labelView: UILabel!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+//    func fruitDidSelect(_ fruit: String) {
+//        labelView.text = fruit
+//    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let ctrl = segue.destination as! AppleViewController
+        ctrl.delegate = self
+    }
+
+}
+
+extension DelegateSecondViewController: AppleViewControllerDelegate{
+
+func fruitDidSelect(_ fruit: String) {
+       labelView.text = fruit
+   }
+}
